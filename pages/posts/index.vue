5bf1a5b96cd8dd9b68ca3cf313a9069cd5287444
Yoshi Rubin <yoshrubin@gmail.com>
2024-04-08 19:07:26 +0300

    load and display individual posts

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,7 +1,32 @@
+<script setup lang="ts">
+const pagination = reactive({
+  limit: 10,
+  offset: 0,
+  order: "newestFirst",
+});
+
+const {
+  data: posts,
+  pending,
+  error,
+  refresh,
+} = await useFetch("/api/posts", {
+  pagination,
+});
+
+console.log(posts.value);
+</script>
+
 <template>
   <div class="h-screen flex justify-center items-center">
-    <div>
-      <h1 class="text-2xl">Display The Paginated Posts Here</h1>
+    <div v-if="pending">Loading...</div>
+    <div v-else class="space-y-5">
+      <div v-for="(post, index) in posts" :key="index">
+        <NuxtImg :src="post.image" />
+        <h2 class="text-xl font-bold">{{ post.title }}</h2>
+      </div>
+    </div>
+    <!-- <h1 class="text-2xl">Display The Paginated Posts Here</h1>
       <ul class="list-disc list-inside ml-10">
         <li>Keep performance in mind</li>
         <li>Make sure to display optimized images</li>
@@ -15,7 +40,6 @@
             go to the next task (displaying the individual post)</NuxtLink
           >
         </li>
-      </ul>
-    </div>
+      </ul> -->
   </div>
 </template>
