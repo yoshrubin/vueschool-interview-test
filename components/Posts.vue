124833b0239562f6dca11ccd8da38bd66a2b1412
Yoshi Rubin <yoshrubin@gmail.com>
2024-04-10 12:06:47 +0300

    Add infinite scroll functionality to Posts component

---
+++ components/Posts.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { useInfiniteScroll } from "@vueuse/core";
+const query = reactive({
+  limit: 10,
+  offset: 0,
+  order: "newestFirst",
+});
+
+const target = ref<HTMLElement | null>(null);
+const posts = ref<Post[]>([]);
+const canLoadMore = ref(true);
+const isLoading = ref(false);
+useInfiniteScroll(
+  target,
+  async () => {
+    query.offset += query.limit;
+    isLoading.value = true;
+    const { data } = await useFetch("/api/posts", {
+      query,
+    });
+    isLoading.value = false;
+    if (data.value?.length) {
+      posts.value.push(...data.value);
+    } else {
+      canLoadMore.value = false;
+    }
+  },
+  { distance: 10, canLoadMore: () => canLoadMore.value }
+);
+</script>
+
+<template>
+  <div class="h-screen flex justify-center items-center flex-col">
+    <div ref="target" class="w-full overflow-auto h-full">
+      <Post :post="post" v-for="(post, index) in posts" :key="index" />
+      <div v-if="isLoading">Loading...</div>
+    </div>
+  </div>
+</template>
