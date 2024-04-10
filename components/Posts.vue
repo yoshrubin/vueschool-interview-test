ebb240030f747adbcd0e7f88e455259cb83b3fc2
Yoshi Rubin <yoshrubin@gmail.com>
2024-04-10 12:31:08 +0300

    refactor design to have scroller outside and add sort radio buttons

--- components/Posts.vue
+++ components/Posts.vue
@@ -14,29 +14,62 @@ useInfiniteScroll(
   target,
   async () => {
     query.offset += query.limit;
-    isLoading.value = true;
-    const { data } = await useFetch("/api/posts", {
-      query,
-    });
-    isLoading.value = false;
-    if (data.value?.length) {
-      posts.value.push(...data.value);
-    } else {
-      canLoadMore.value = false;
-    }
+    await loadData();
   },
-  { distance: 10, canLoadMore: () => canLoadMore.value }
+  { distance: 100, canLoadMore: () => canLoadMore.value }
+);
+
+async function loadData() {
+  isLoading.value = true;
+  const { data } = await useFetch("/api/posts", {
+    query,
+  });
+  isLoading.value = false;
+  if (data.value?.length) {
+    posts.value.push(...data.value);
+  } else {
+    canLoadMore.value = false;
+  }
+}
+
+watch(
+  () => query.order,
+  () => {
+    posts.value = [];
+    query.offset = 0;
+    canLoadMore.value = true;
+    loadData();
+  }
 );
 </script>
 
 <template>
-  <div class="h-screen flex justify-center items-center flex-col">
-    <div
-      ref="target"
-      class="overflow-auto h-full grid grid-cols-2 gap-4 px-8 mx-auto max-w-4xl"
-    >
-      <Post :post="post" v-for="(post, index) in posts" :key="index" />
-      <div v-if="isLoading">Loading...</div>
+  <div
+    ref="target"
+    class="h-screen flex justify-center items-center flex-col overflow-auto"
+  >
+    <div class="h-full p-10 flex flex-col">
+      <div class="flex items-center mb-4">
+        <input
+          v-model="query.order"
+          type="radio"
+          value="newestFirst"
+          id="newestFirst"
+        />
+        <label class="ml-2" for="newestFirst">Newest First</label>
+        <input
+          class="ml-4"
+          v-model="query.order"
+          type="radio"
+          value="oldestFirst"
+          id="oldestFirst"
+        />
+        <label class="ml-2" for="oldestFirst">Oldest First</label>
+      </div>
+      <div class="grid grid-cols-2 gap-4 mx-auto max-w-4xl">
+        <Post :post="post" v-for="(post, index) in posts" :key="index" />
+        <div v-if="isLoading">Loading...</div>
+      </div>
     </div>
   </div>
 </template>
