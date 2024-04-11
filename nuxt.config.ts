4a2a63db158dcbbc7ff9ea2703e2c6899e8710e4
Yoshi Rubin <yoshrubin@gmail.com>
2024-04-11 12:17:41 +0300

    Add SEO module to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  site: {
+    url: "https://vueschoolblog.com",
+    name: "Awesome Nuxt Blog",
+    description: "Welcome to my nuxt site!",
+    defaultLocale: "en",
+  },
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/image"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "@nuxtjs/seo"],
   nitro: {
     experimental: {
       database: true,
