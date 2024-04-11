765f27e03d0918c7c1e748a9379a049995fac259
Yoshi Rubin <yoshrubin@gmail.com>
2024-04-11 11:40:06 +0300

    fix post layout

--- components/Post.vue
+++ components/Post.vue
@@ -8,12 +8,10 @@ const datePosted = post?.publishedAt
   ? new Date(post.publishedAt).toLocaleDateString()
   : "";
 
-const formattedDate = useDateFormat(datePosted, "MMMM Do, YYYY");
+const formattedDate = useDateFormat(post?.publishedAt, "MMMM Do, YYYY");
 </script>
 <template>
-  <main
-    class="h-screen flex justify-center items-center flex-col overflow-auto"
-  >
+  <div class="min-h-screen">
     <header>
       <NuxtImg
         v-if="post"
@@ -24,25 +22,27 @@ const formattedDate = useDateFormat(datePosted, "MMMM Do, YYYY");
       />
       <div v-else class="w-full h-96 bg-gray-300 animate-pulse" />
     </header>
-    <section class="p-10 max-w-4xl mx-auto space-y-4">
-      <h1 class="text-4xl font-bold">{{ post?.title }}</h1>
-      <p class="text-gray-500">{{ post?.excerpt }}</p>
-      <div class="flex items-center space-x-2">
-        <NuxtImg
-          v-if="post"
-          class="w-16 h-16 rounded-full"
-          :src="post.user.avatar"
-          loading="lazy"
-          :alt="`${post.user.firstName} ${post.user.lastName}`"
-        />
-        <div>
-          <p>{{ post?.user.firstName }} {{ post?.user.lastName }}</p>
-          <p class="text-sm text-gray-700">
-            {{ new Date(formattedDate).toLocaleDateString() }}
-          </p>
+    <main class="flex justify-center items-center flex-col overflow-auto">
+      <section class="p-10 max-w-4xl mx-auto space-y-4">
+        <h1 class="text-4xl font-bold">{{ post?.title }}</h1>
+        <p class="text-gray-500">{{ post?.excerpt }}</p>
+        <div class="flex items-center space-x-2">
+          <NuxtImg
+            v-if="post"
+            class="w-16 h-16 rounded-full"
+            :src="post.user.avatar"
+            loading="lazy"
+            :alt="`${post.user.firstName} ${post.user.lastName}`"
+          />
+          <div>
+            <p>{{ post?.user.firstName }} {{ post?.user.lastName }}</p>
+            <p class="text-sm text-gray-700">
+              {{ new Date(formattedDate).toLocaleDateString() }}
+            </p>
+          </div>
         </div>
-      </div>
-      <article v-html="post?.content" />
-    </section>
-  </main>
+        <article v-html="post?.content" />
+      </section>
+    </main>
+  </div>
 </template>
