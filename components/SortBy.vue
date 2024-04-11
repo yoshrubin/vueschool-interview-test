1185ba77ee6bebd5ef9405b88356d3319e2fa1db
Yoshi Rubin <yoshrubin@gmail.com>
2024-04-11 11:27:09 +0300

    Add SortBy component for sorting posts

---
+++ components/SortBy.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const order = defineModel("order");
+</script>
+<template>
+  <section class="flex items-center mb-4">
+    <input v-model="order" type="radio" value="newestFirst" id="newestFirst" />
+    <label class="ml-2" for="newestFirst">Newest First</label>
+    <input
+      class="ml-4"
+      v-model="order"
+      type="radio"
+      value="oldestFirst"
+      id="oldestFirst"
+    />
+    <label class="ml-2" for="oldestFirst">Oldest First</label>
+  </section>
+</template>
