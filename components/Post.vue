d8caba9e249a727b7ba0cc970090255e7f1a160d
Yoshi Rubin <yoshrubin@gmail.com>
2024-04-11 11:26:54 +0300

    move sorting to a component

---
+++ components/Post.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import type { PostWithUser } from "~/types";
+import { useDateFormat } from "@vueuse/core";
+
+const { post } = defineProps<{ post?: PostWithUser }>();
+
+const datePosted = post?.publishedAt
+  ? new Date(post.publishedAt).toLocaleDateString()
+  : "";
+
+const formattedDate = useDateFormat(datePosted, "MMMM Do, YYYY");
+</script>
+<template>
+  <main
+    class="h-screen flex justify-center items-center flex-col overflow-auto"
+  >
+    <header>
+      <NuxtImg
+        v-if="post"
+        :src="post.image"
+        class="w-full h-96 object-cover"
+        loading="lazy"
+        :alt="post.title"
+      />
+      <div v-else class="w-full h-96 bg-gray-300 animate-pulse" />
+    </header>
+    <section class="p-10 max-w-4xl mx-auto space-y-4">
+      <h1 class="text-4xl font-bold">{{ post?.title }}</h1>
+      <p class="text-gray-500">{{ post?.excerpt }}</p>
+      <div class="flex items-center space-x-2">
+        <NuxtImg
+          v-if="post"
+          class="w-16 h-16 rounded-full"
+          :src="post.user.avatar"
+          loading="lazy"
+          :alt="`${post.user.firstName} ${post.user.lastName}`"
+        />
+        <div>
+          <p>{{ post?.user.firstName }} {{ post?.user.lastName }}</p>
+          <p class="text-sm text-gray-700">
+            {{ new Date(formattedDate).toLocaleDateString() }}
+          </p>
+        </div>
+      </div>
+      <article v-html="post?.content" />
+  </main>
+</template>
--- components/Posts.vue
+++ components/Posts.vue
@@ -66,23 +66,7 @@ onMounted(() => {
   >
     <section v-if="isLoading && !posts.length">Loading...</section>
     <section v-else class="h-full p-10 flex flex-col">
-      <section class="flex items-center mb-4">
-        <input
-          v-model="query.order"
-          type="radio"
-          value="newestFirst"
-          id="newestFirst"
-        />
-        <label class="ml-2" for="newestFirst">Newest First</label>
-        <input
-          class="ml-4"
-          v-model="query.order"
-          type="radio"
-          value="oldestFirst"
-          id="oldestFirst"
-        />
-        <label class="ml-2" for="oldestFirst">Oldest First</label>
-      </section>
+      <SortBy v-model:order="query.order" />
       <section class="grid grid-cols-2 gap-4 mx-auto max-w-4xl">
         <PostCard :post="post" v-for="post in posts" :key="post.id" />
         <div v-if="isLoading">Loading...</div>
