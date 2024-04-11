88ecd24064b6a38a97e6ab260878a612694f4297
Yoshi Rubin <yoshrubin@gmail.com>
2024-04-11 11:27:34 +0300

    load post by slug and handle errors

--- pages/posts/[slug].vue
+++ pages/posts/[slug].vue
@@ -1,12 +1,14 @@
+<script setup lang="ts">
+import type { PostWithUser } from "~/types";
+
+const { data: post, error } = useFetch<PostWithUser>("/api/posts/:slug");
+if (error.value) {
+  showError(error.value);
+}
+if (!post.value) {
+  showError("Post not found");
+}
+</script>
 <template>
-  <div class="h-screen flex justify-center items-center">
-    <div>
-      <h1 class="text-2xl">Display the Individual Post Here</h1>
-      <ul class="list-disc list-inside ml-10">
-        <li>Keep seo in mind</li>
-        <li>Make sure to display optimized images</li>
-        <li>Make it look good 💪</li>
-      </ul>
-    </div>
-  </div>
+  <Post :post="post" />
 </template>
